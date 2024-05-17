070d123bfb85d6f4de1ce33d7a87db83f26a5775
dfh5g <drnunessopsg@hotmail.com>
2024-05-17 14:07:01 +0700

    test project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("world");
+function connect(root) {
+  if (!root) return root;
+  let levelStart = root;
+  while (levelStart) {
+    let curr = levelStart;
+    while (curr) {
+      if (curr.left) curr.left.next = curr.right;
+      if (curr.right && curr.next) curr.right.next = curr.next.left;
+      curr = curr.next;
+    }
+    levelStart = levelStart.left;
+  }
+  return root;
+}
